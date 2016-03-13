a9cf5f11c45221889d99c07985b013bd0ec27716
James Traver <jtraverjr@gmail.com>
2016-03-13 17:43:09 -0500

    initial tests

---
+++ spec/HapiServerStartSpec.js
@@ -0,0 +1,23 @@
+/**
+ * Created by codeforcoffee on 3/13/16.
+ */
+'use strict';
+describe('DotEnv KeyValuePair Tests', function () {
+    var app;
+    beforeEach(function() {
+        app = require('../app');
+    });
+    it('should start and be hosted', function() {
+        console.log(app.info);
+        expect(app.info.uri).toBeTruthy();
+    });
+    it('should be listening on an open port', function() {
+        expect(app.info.port).toBeTruthy();
+    });
+    it('should be be provided a process id', function() {
+        expect(app.info.id).toBeTruthy();
+    });
+    afterEach(function() {
+        app.stop();
+    });
+});
